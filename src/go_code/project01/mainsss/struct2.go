3095ba8842a4b8b98f16518db49dbce2883ac782
php-Carl-at-289631370416 <455549675@qq.com>
2022-01-06 17:32:14 +0800

    修嘎

--- src/go_code/project01/mainsss/struct2.go
+++ src/go_code/project01/mainsss/struct2.go
@@ -7,17 +7,15 @@ import (
 
 type Person struct {
 	Name string `json:"name"`
-	Age int8
-
+	Age  int8
 }
 
 type Teststr struct {
 	Name string
-	Age int8
-
+	Age  int8
 }
 
-func main()  {
+func main() {
 
 	//方式1
 	var t0 Person
@@ -26,13 +24,11 @@ func main()  {
 	fmt.Println(t0)
 
 	//方式2
-	t1 := Person{"曹磊",20}
+	t1 := Person{"曹磊", 20}
 	fmt.Println(t1)
 
-
-
 	//方式3
-	var t2 *Person = new (Person)
+	var t2 *Person = new(Person)
 	(*t2).Name = "嘟嘟"
 	(*t2).Age = 12
 	//简化也可以这样写
@@ -40,18 +36,15 @@ func main()  {
 	t2.Age = 23
 	fmt.Println(*t2)
 
-
 	//方式4
 	var t3 *Person = &Person{}
 	t3.Name = "都的"
 	t3.Age = 23
 	fmt.Println(*t3)
 
-
 	//装换json格式
-	t3joson,_ := json.Marshal(t3)
+	t3joson, _ := json.Marshal(t3)
 	fmt.Println(string(t3joson))
-
 
 	//使用结构体方法
 	s3 := t1.test()
@@ -62,12 +55,13 @@ func main()  {
 	fmt.Println(t222)
 
 }
+
 //结构体方法的声明
-func (p Person)test()string  {
+func (p Person) test() string {
 	return p.Name
 }
 
-func (t Teststr)test2(str string)string  {
-	t.Name = "cccccccccccccc"
-	return  t.Name
+func (t Teststr) test2(str string) string {
+	t.Name = "cccccccccccccc2222222"
+	return t.Name
 }
